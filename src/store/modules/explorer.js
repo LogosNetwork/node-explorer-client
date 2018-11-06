496e08b8fbe0a122e42547dfa3b7c0c7636ecd6a
Tyler Storm <stormtv@mail.uc.edu>
2018-11-06 16:38:12 -0500

    Add option to query batchBlocks for transaction history over cacheDB

--- src/store/modules/explorer.js
+++ src/store/modules/explorer.js
@@ -16,10 +16,24 @@ const getters = {
 
 const actions = {
   getRecentTransactions: ({ commit }) => {
-    rpcClient.batchBlocks.history(1, 0).then(val => {
+    rpcClient.batchBlocks.history(50, 0).then(val => {
       if (val) {
         if (!val.error) {
           commit('setBatchBlock', val.batch_blocks[0])
+          // let batchBlock = cloneDeep(val.batch_blocks[0])
+          // let cachedTransactions = []
+          // for (let i = 0; i < batchBlock.length; i++) {
+          //   for (let n = 0; n < batchBlock[i].blocks.length; n++) {
+          //     batchBlock[i].blocks[n].type = 'send'
+          //     batchBlock[i].blocks[n].amount = parseFloat(Number(rpcClient.convert.fromReason(batchBlock[i].blocks[n].amount, 'LOGOS')).toFixed(5))
+          //     batchBlock[i].blocks[n].timestamp = parseInt(batchBlock[i].blocks[n].timestamp)
+          //     cachedTransactions.unshift(batchBlock[i].blocks[n])
+          //     if (i === batchBlock.length - 1 &&
+          //         n === batchBlock[i].blocks.length - 1) {
+          //       commit('setTransactions', cachedTransactions)
+          //     }
+          //   }
+          // }
         } else {
           commit('setError', val.error)
         }
@@ -88,9 +102,7 @@ const mutations = {
     let blockData = cloneDeep(block)
     if (blockData.type === 'send') {
       blockData.amount = parseFloat(Number(rpcClient.convert.fromReason(blockData.amount, 'LOGOS')).toFixed(5))
-      blockData.account = blockData.account
       blockData.timestamp = parseInt(blockData.timestamp)
-      blockData.link_as_account = blockData.link_as_account
       state.transactions.unshift(blockData)
     }
   }
