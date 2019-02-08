689ac0f49ec702daa7a8a76062cdda10693aa150
Tyler Storm <stormtv@mail.uc.edu>
2019-02-07 23:16:36 -0500

    Add proper regex support for new bsb delegate ids

--- src/store/modules/mqtt.js
+++ src/store/modules/mqtt.js
@@ -21,6 +21,7 @@ const actions = {
       })
       const accountMqttRegex = mqttRegex('account/+account').exec
       const transactionMqttRegex = mqttRegex('transaction/+hash').exec
+      const batchblockMqttRegex = mqttRegex('batchblock/+delegateid').exec
       client.on('message', (topic, message) => {
         message = JSON.parse(message.toString())
         // TODO Eventually validate the signatures of the blocks to be "trustless"
@@ -30,7 +31,7 @@ const actions = {
         } else if (topic === 'epoch') {
           commit('chains/addEpoch', message, { root: true })
           commit('explorer/setEpoch', message, { root: true })
-        } else if (topic === 'batchBlock') {
+        } else if (batchblockMqttRegex(topic)) {
           commit('chains/addBatchBlock', message, { root: true })
           commit('explorer/setBatchBlock', message, { root: true })
         } else {
