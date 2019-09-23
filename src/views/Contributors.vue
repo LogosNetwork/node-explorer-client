a0352935f0067c87778e630d69e4871d2af19c45
Tyler Storm <stormtv@mail.uc.edu>
2019-09-23 01:14:16 -0400

    Contributors and Home page

--- src/views/Contributors.vue
+++ src/views/Contributors.vue
@@ -1,15 +1,137 @@
 <template>
   <div id="primary">
     <b-container class="pt-5">
-      <h5 class="text-left d-block d-sm-none">Contributors</h5>
-      <h2 class="d-none d-sm-block">Contributors</h2>
+      <h2 class="mb-5">Contributors</h2>
+      <b-row>
+        <b-col
+          v-for="contributor in contributors"
+          :key="contributor.name"
+          cols="12"
+          md="6"
+          xl="3"
+          class="mb-4"
+        >
+          <b-card
+            no-body
+            class="boder-0 shadow h-100"
+          >
+            <img :src="`/team/${contributor.image}`" class="card-img-top" :alt="`${contributor.name}'s photo`">
+            <b-card-body class="text-center">
+              <h5 class="card-title mb-0">{{ contributor.name }}</h5>
+              <div class="card-text text-black-50">{{ contributor.title }}</div>
+              <div class="text-left">
+                <a
+                  v-if="contributor.github"
+                  :href="contributor.github"
+                  target="_blank"
+                >
+                  <font-awesome-icon size="lg" class="align-middle" :icon="faGithub" />
+                  <span class="sr-only">Github</span>
+                </a>
+                <a
+                  v-if="contributor.linkedin"
+                  :href="contributor.linkedin"
+                  target="_blank"
+                  :class="contributor.github ? 'ml-3' : ''"
+                >
+                  <font-awesome-icon size="lg" class="align-middle" :icon="faLinkedin" />
+                  <span class="sr-only">Linkedin</span>
+                </a>
+              </div>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
+import { BCardBody } from 'bootstrap-vue'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 export default {
-  name: 'Contributors'
+  name: 'Contributors',
+  data () {
+    return {
+      faLinkedin,
+      faGithub,
+      contributors: [
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Carl Hua',
+          image: 'carl.jpeg',
+          title: 'Logos Engineering Lead',
+          github: 'https://www.github.com/carlhua',
+          linkedin: 'https://www.linkedin.com/in/carl-hua-4b309113/'
+        },
+        {
+          name: 'Tyler Storm',
+          image: 'tyler.jpg',
+          title: 'Full Stack Software Engineer',
+          github: 'https://www.github.com/stormtv',
+          linkedin: 'https://www.linkedin.com/in/stormtv/'
+        }
+      ]
+    }
+  },
+  components: {
+    BCardBody
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,94 @@
 <template>
   <div id="primary">
     <b-container class="pt-5">
-      <h5 class="text-left d-block d-sm-none">Logos Network</h5>
-      <h2 class="d-none d-sm-block">Logos Network</h2>
+      <font-awesome-icon size="3x" class="logosLogo align-middle mb-3" :icon="faLambda" />
+      <h2>Logos Network</h2>
+      <b-button
+        class="mt-3"
+        variant="outline-primary"
+        href="/whitepaper.pdf"
+      >
+        Whitepaper
+      </b-button>
+      <b-button
+        class="ml-3 mt-3"
+        variant="outline-primary"
+        href="https://github.com/logosNetwork"
+        target="_blank"
+      >
+        Source Code
+      </b-button>
+      <b-button
+        class="ml-3 mt-3"
+        variant="outline-primary"
+        href="https://github.com/LogosNetwork/logos-core/wiki"
+        target="_blank"
+      >
+        Design Docs
+      </b-button>
+      <b-button
+        class="ml-3 mt-3"
+        variant="outline-primary"
+        href="https://logosnetwork.atlassian.net/wiki/spaces/LOGOS/pages/274071553/Logos+-+Notes+to+future+developers"
+        target="_blank"
+      >
+        Deployment Docs
+      </b-button>
+      <div class="text-left mt-5">
+        <h2>Overview</h2>
+        <p>Logos is a high performance scalable cryptocurrency designed for real time payments. Our novel consensus, Axios PBFT, provides decentralization, fast settlement times, high throughput, and transaction immuatability</p>
+        <!-- <h3>Parallelization / Concurrency</h3>
+        <p>
+          In order to achieve this performance we focus on paralleizing our consensus so that multiple rounds of concensus can take place at the same time.
+        </p>
+        <h3>Send & Receive Chain</h3>
+        <p>
+          Every account in logos network has its own chains. This allows each account to be updated quickly and asynchronously independently to the rest of the network.
+        </p>
+        <h3>Logos Accounts</h3>
+        <p>
+          Logos accounts are the standard accounts on the Logos Network. See <a href="https://github.com/LogosNetwork/SDF/blob/master/design/original_g_drive/Design/Tokens/AccountChains.md" target="_blank">types of blocks associated to a logos account.</a>
+        </p>
+        <h3>Token Accounts</h3>
+        <p>
+          Token accounts are the ledger account of a token where all the history of transactions performed on the token are contained. See <a href="https://github.com/LogosNetwork/SDF/blob/master/design/original_g_drive/Design/Tokens/AccountChains.md" target="_blank">types of blocks associated to a token account.</a>
+        </p>
+        <h3>Confirmation Process & Governance</h3>
+        <h4>Axios PBFT</h4>
+        <h4>BLS</h4>
+        <h4>Elections</h4>
+        <h4>Staking</h4>
+        <h4>Rewards</h4>
+        <h4>Fees</h4>
+        <h3>Epoch Blocks</h3>
+        <h3>Micro Epoch Blocks</h3>
+        <h3>Request Blocks</h3>
+        <h3>Epoch transition</h3>
+        <h3>Tx Acceptors and DDoS prevention</h3>
+        <h3>P2P</h3>
+        <h3>Bootstrapping</h3>
+        <h3>Identity Management</h3>
+        <h3>Automated Testing Infrastructure</h3> -->
+      </div>
     </b-container>
   </div>
 </template>
 
 <script>
+import { faLambda } from '@fortawesome/pro-light-svg-icons'
+
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      faLambda
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+.logosLogo {
+  color: #4668da;
+}
 </style>
